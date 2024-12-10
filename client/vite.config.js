774e74baae4f39f0aa5ddd76566f7e16506d2366
creativeCode90 <jcoder967@gmail.com>
2024-12-10 12:50:53 +0530

    commetns done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-
+// this is vite config file in this file satup my spring boot backend url proxy for interect with my spring boot
 export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8080/app',
+                target: 'http://localhost:8080/app', // my spring boot backend url
                 changeOrigin: true,
                 secure: false,
             },
